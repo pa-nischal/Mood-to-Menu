b5954e906663ca88b762bb1c8af9e072ce3e60ae
Nischal <nischal0608@gmail.com>
2026-01-10 01:33:19 +0545

    Add files via upload

---
+++ Profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.write("# Profile")
+"""
+My name is Nischal Pokharel. I'm a student at Kathmandu University, currently studying B.Tech in AI. This page is my Python AI Web App Portfolio. \n
+Contact me: \n
+Phone - (+977) 9745991136 \n
+Email - nischal0608@gmail.com \n
+Linkedin - https://www.linkedin.com/in/nischal-pokharel-300549368/ \n
+"""
